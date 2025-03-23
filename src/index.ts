dca6c85b7f74124e49569f0f40256d9bc77d16f6
Arpit <ak30062004@gmail.com>
2025-03-23 12:01:04 +0530

    Upload endpoint setup + minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from "express";
+import { generate } from "./helper/functions";
+import cors from "cors";
+import simpleGit from "simple-git";
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.post("/repo-url", (req: Request,res: Response) => {
-    const {repoUrl} = req.body;
-    
+app.post("/repo-url", async (req: Request, res: Response) => {
+  const { repoUrl } = req.body;
+  const session = generate();
+  await simpleGit().clone(repoUrl, `./output/${session}`);
+  res.json({
+    id: session,
+  });
+});
 
-})
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
